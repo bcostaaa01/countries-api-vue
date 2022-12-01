325ab61c2cd4c9274cd02687bbcd4d03c7cf3cad
Bruno Miguel Alves Costa <brunomiguelalvescosta@Bruno-Costas-MacBook.local>
2022-12-01 16:50:09 +0000

    Render data in seperate card elements, names of the countries,flags and currencies

--- countries-api/src/App.vue
+++ countries-api/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Hello world!</h1>
+  <h1>Countries API</h1>
   <ApiData />
 </template>
 
--- countries-api/src/components/ApiData.vue
+++ countries-api/src/components/ApiData.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    <div v-for="(item, index) in data" :key="index">{{ item.name.common }}</div>
+    <div v-for="(item, index) in data" :key="index">
+      <div id="country-card">
+        <div class="header">
+          <h1>{{ item.name.common }}</h1>
+        </div>
+        <div>
+          {{ item.name.common }}
+          <div>Currency: {{ item.currencies }}</div>
+        </div>
+        {{ item.flag }}
+      </div>
+    </div>
     <button @click="getData">get data</button>
   </div>
 </template>
@@ -16,9 +27,7 @@ export default {
   methods: {
     async getData() {
       try {
-        const response = await fetch(
-          "https://restcountries.com/v3.1/all"
-        );
+        const response = await fetch("https://restcountries.com/v3.1/all");
 
         this.data = await response.json();
       } catch (err) {
@@ -28,3 +37,29 @@ export default {
   },
 };
 </script>
+
+<style>
+.card {
+  padding: 2px 16px;
+  align-content: center;
+}
+
+#country-card {
+  margin: 20px;
+  padding: 20px;
+  width: 500px;
+  min-height: 200px;
+  display: grid;
+  grid-template-rows: 20px 50px 1fr 50px;
+  border-radius: 10px;
+  box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.25);
+  transition: all 0.2s;
+}
+
+.header {
+  height: 30%;
+  background: pink;
+  color: white;
+  text-align: center;
+}
+</style>
